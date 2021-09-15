35712a6355dd2cb25613c093021c2c9c879edaf9
Yassine Fikri <ysn.fikri@gmail.com>
2021-09-15 17:12:17 +0100

    react to profile username change

--- assets/components/Profile.vue
+++ assets/components/Profile.vue
@@ -23,36 +23,26 @@ import axios from "axios";
 import CoverAvatar from "./Partials/CoverAvatar";
 import NewPostForm from "./Post/NewPostForm";
 import MiniProfileInfos from "./Partials/MiniProfileInfos";
+
 export default {
   name: "Profile",
-  components: {CoverAvatar,MiniProfileInfos,NewPostForm,PostList},
+  components: {CoverAvatar, MiniProfileInfos, NewPostForm, PostList},
   props: ['username'],
-  data(){
+  data() {
     return {
-      posts : [],
+      posts: [],
       userInfos: null
     }
   },
   beforeMount() {
-    if(undefined !== this.username) {
-      axios
-          .get(this.$Routing.generate('user_infos_username', {'username': this.username}))
-          .then(response => {
-            this.userInfos = response.data
-          })
-          .catch(error => {
-            console.log(error)
-          })
-    } else {
-      this.userInfos = this.$store.state.userInfos
-    }
+    this.init()
   },
   mounted() {
     this.fetchPosts()
   },
   methods: {
-    fetchPosts(){
-      let route = undefined === this.username ? this.$Routing.generate('profile') : this.$Routing.generate('user_profile', {'username' : this.username})
+    fetchPosts() {
+      let route = undefined === this.username ? this.$Routing.generate('profile') : this.$Routing.generate('user_profile', {'username': this.username})
       axios
           .get(route)
           .then(response => {
@@ -61,8 +51,28 @@ export default {
           .catch(error => {
             console.log(error)
           })
+    },
+    init() {
+      if (undefined !== this.username) {
+        axios
+            .get(this.$Routing.generate('user_infos_username', {'username': this.username}))
+            .then(response => {
+              this.userInfos = response.data
+            })
+            .catch(error => {
+              console.log(error)
+            })
+      } else {
+        this.userInfos = this.$store.state.userInfos
+      }
     }
-  }
+  },
+  watch: {
+    '$route.params.username': function (username) {
+      this.init()
+      this.fetchPosts()
+    }
+  },
 }
 </script>
 
