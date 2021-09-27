aa72ba4aa044aaa19a22256e3d556308bec72429
Yassine Fikri <ysn.fikri@gmail.com>
2021-09-27 14:28:00 +0100

    add friendship toasts

---
+++ assets/components/MyBootrsrap/MyToast.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="toast-container position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+    <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true"
+         :class="[{'text-white border-0': variant !== 'light'}, 'bg-'+variant]" style="z-index: 11">
+      <div class="toast-header">
+        <div class="toast-avatar-container me-2">
+          <img :src="user.avatar_url" class="rounded-circle w-100 h-100" :alt="user.firstName + ' ' + user.lastName">
+        </div>
+        <strong class="me-auto">{{ user.firstName }} {{ user.lastName }}</strong>
+        <small>{{ time|momentAgo }}</small>
+        <button type="button" @click="destroySelf" class="btn-close" :class="{'btn-close-white': variant !== 'light'}"
+                data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <div class="toast-body">
+        {{ content }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-toast",
+  props: ['user', 'content', 'time', 'variant'],
+  methods: {
+    destroySelf() {
+      this.$destroy()
+      this.$el.parentNode.removeChild(this.$el);
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.destroySelf()
+    }, 5000);
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
