c27efe05f0f6f8d18646f1dd74252a39daafd273
Yassine Fikri <ysn.fikri@gmail.com>
2021-10-05 09:14:46 +0100

    improve layout

--- assets/components/Chat/ConversationLink.vue
+++ assets/components/Chat/ConversationLink.vue
@@ -1,20 +1,22 @@
 <template>
   <li class="list-group-item" :class="[active ? 'active' : '']">
-    <a class="d-flex justify-content-between align-items-start" aria-current="page" :href="href">
-      <div class="d-flex flex-column me-4">
-        <div v-for="(participant,index2) in item.participants" :key="index2">
-          <div v-if="participant.username !== getUsername" class="d-flex my-1 align-items-center">
-            <div  class="toast-avatar-container me-3">
-              <img :src="participant.avatar_url" class="rounded-circle w-100 h-100" :alt="participant.firstName + ' ' + participant.lastName">
+    <a class="d-flex flex-column" aria-current="page" :href="href">
+      <div class="d-flex justify-content-between align-items-start">
+        <div class="d-flex flex-column me-4 flex-grow-1">
+          <div v-for="(participant,index2) in item.participants" :key="index2">
+            <div v-if="participant.username !== getUsername" class="d-flex my-1 align-items-center">
+              <div  class="toast-avatar-container me-3">
+                <img :src="participant.avatar_url" class="rounded-circle w-100 h-100" :alt="participant.firstName + ' ' + participant.lastName">
+              </div>
+              <span class="fw-bold">{{ participant.firstName }} {{ participant.lastName }}</span>
             </div>
-            <span class="fw-bold">{{ participant.firstName }} {{ participant.lastName }}</span>
           </div>
         </div>
-        <span v-if="item.updatedAt" class="me-2">{{ item.updatedAt | momentAgo }}</span>
+        <div class="ms-auto my-auto">
+          <span class="badge bg-danger rounded-pill my-auto" :class="[unreadCount ? 'visible' : 'invisible']">{{ unreadCount }}</span>
+        </div>
       </div>
-      <div class="ms-auto my-auto">
-        <span class="badge bg-danger rounded-pill my-auto" :class="[unreadCount ? 'visible' : 'invisible']">{{ unreadCount }}</span>
-      </div>
+      <span v-if="item.updatedAt" class="me-2 align-self-end">{{ item.updatedAt | momentAgo }}</span>
     </a>
   </li>
 </template>
