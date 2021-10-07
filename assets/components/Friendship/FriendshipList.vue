34d7b49591141ca5d806616db99a0a88cedb184d
Yassine Fikri <ysn.fikri@gmail.com>
2021-10-07 15:28:13 +0100

    add friendships filter

--- assets/components/Friendship/FriendshipList.vue
+++ assets/components/Friendship/FriendshipList.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="my-container-600 mx-auto py-5">
+    <div class="form-group mb-3">
+      <input v-model="query" class="form-control" type="text" placeholder="search">
+    </div>
     <div id="search_results">
       <ul class="list-group" v-if="Object.keys(friendships).length > 0">
-        <li v-for="(friendship,index) in friendships" class="list-group-item d-flex align-content-center">
+        <li v-for="(friendship,index) in friendships" v-if="'' === query || true === filterResult(friendship)" class="list-group-item d-flex align-content-center">
           <navbar-search-link-container :user="getFriendshipUser(friendship)" :key="index" class="flex-grow-1"/>
           <i class="bi bi-clock ms-3 my-auto" style="font-size: 20px"></i>
         </li>
@@ -24,9 +27,18 @@ import {mapGetters} from 'vuex'
 export default {
   name: "navbar-friendship-list",
   components: {NavbarSearchLinkContainer},
+  data() {
+    return {
+      query: ''
+    }
+  },
   methods: {
     getFriendshipUser(friendship) {
       return friendship.sender.username === this.username ? friendship.receiver : friendship.sender
+    },
+    filterResult(friendship) {
+      let user = (this.username === friendship.sender.username) ? friendship.receiver.username : friendship.sender.username
+      return this.filterRegex.test(user)
     }
   },
   computed: {
@@ -34,6 +46,9 @@ export default {
       'username',
       'friendships'
     ]),
+    filterRegex(){
+      return new RegExp(this.query)
+    }
   }
 }
 </script>
