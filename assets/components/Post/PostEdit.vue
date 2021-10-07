227cfd91a064a870c56817f3cab120d1e34ed9bf
Yassine Fikri <ysn.fikri@gmail.com>
2021-10-07 11:00:58 +0100

    add post image delete buttons to front

--- assets/components/Post/PostEdit.vue
+++ assets/components/Post/PostEdit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-5">
+  <div class="py-5" @click="handleClick">
     <post-form :postId="postId"/>
   </div>
 </template>
@@ -10,7 +10,19 @@ import PostForm from "./PostForm";
 export default {
   name: "post-edit",
   components: {PostForm},
-  props: ['postId']
+  props: ['postId'],
+  methods: {
+    handleClick(e) {
+      if (true === e.target.matches('button.btn.btn-danger.post-image-delete-button')
+          || true === e.target.matches('i.bi.bi-trash.post-image-delete-button-icon')) {
+        this.deletePostImage(e.target.getAttribute('data-id'))
+      }
+    },
+    deletePostImage(id) {
+      let elementId = 'post-image-' + id
+      this.$el.querySelectorAll('#' + elementId + ',#preview-' + elementId).forEach((element) => element.remove())
+    }
+  }
 }
 </script>
 
