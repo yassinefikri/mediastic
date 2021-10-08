5ab31f72567dca59aa5a5af7da3f4bdd7cc1b6c7
Yassine Fikri <ysn.fikri@gmail.com>
2021-10-08 15:41:34 +0100

    decomposing into small components

---
+++ assets/components/Chat/MessageSeen.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="seen-container d-flex"
+       :class="[message.sender.username === username ? 'justify-content-end' : 'justify-content-start']">
+    <div v-for="(person,secondIndex) in message.seenBy"
+         v-if="person.username !== username && lastSeenMessage(message.conversation.id)[person.username] === message.id"
+         :key="secondIndex" class="img-container">
+      <user-avatar :user="person"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex"
+import UserAvatar from "../User/UserAvatar"
+
+export default {
+  name: "message-seen",
+  props: ['message'],
+  components: {UserAvatar},
+  computed: {
+    ...mapGetters([
+      'username',
+      'lastSeenMessage',
+    ]),
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
