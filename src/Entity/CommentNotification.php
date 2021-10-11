a675121acc17b5a669f2abd0b16d2ef9ca30fbc2
Yassine Fikri <ysn.fikri@gmail.com>
2021-10-11 11:02:47 +0100

    remove useless attribute

--- src/Entity/CommentNotification.php
+++ src/Entity/CommentNotification.php
@@ -12,23 +12,11 @@ use Symfony\Component\Serializer\Annotation\Groups;
 class CommentNotification extends AbstractNotification
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private ?int $id = null;
-
-    /**
      * @ORM\ManyToOne(targetEntity=Post::class, inversedBy="commentNotifications")
      * @ORM\JoinColumn(nullable=false)
      * @Groups("notif")
      */
     private ?Post $post;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getPost(): ?Post
     {
