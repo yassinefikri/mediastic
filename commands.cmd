40b38612b74a6c567e4aa9b7c36c20a0f4fdf108
Yassine Fikri <49125227+yassinefikri@users.noreply.github.com>
2022-03-25 10:33:56 +0100

    specified container names to fix commands

--- commands.cmd
+++ commands.cmd
@@ -1,7 +1,7 @@
 @echo off
-doskey composer=docker exec -it --user www-data symfony-vue_php-apache2_1 sh -c "COMPOSER_MEMORY_LIMIT=-1 composer $*"
-doskey console=docker exec -it --user www-data symfony-vue_php-apache2_1 sh -c "php bin/console $*"
-doskey mysql=docker exec -it symfony-vue_mysql_1 sh -c "mysql --user=root --password=root --database symfony-vue"
-doskey yarn=docker exec -it --user node symfony-vue_webpack_1 sh -c "yarn $*"
-doskey phpstan=docker exec -it --user www-data symfony-vue_php-apache2_1 sh -c "vendor/bin/phpstan analyse src tests --level 8"
-doskey dump-routes=docker exec -it --user www-data symfony-vue_php-apache2_1 sh -c "php bin/console fos:js-routing:dump --format=json --target=public/js/fos_js_routes.json"
+doskey composer=docker exec -it --user www-data mediastic_php_apache2 sh -c "COMPOSER_MEMORY_LIMIT=-1 composer $*"
+doskey console=docker exec -it --user www-data mediastic_php_apache2 sh -c "php bin/console $*"
+doskey mysql=docker exec -it mediastic_mysql sh -c "mysql --user=root --password=root --database symfony-vue"
+doskey yarn=docker exec -it --user node mediastic_webpack sh -c "yarn $*"
+doskey phpstan=docker exec -it --user www-data mediastic_php_apache2 sh -c "vendor/bin/phpstan analyse src tests --level 8"
+doskey dump-routes=docker exec -it --user www-data mediastic_php_apache2 sh -c "php bin/console fos:js-routing:dump --format=json --target=public/js/fos_js_routes.json"
